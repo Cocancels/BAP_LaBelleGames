3c561eea7e3c2e5354b9c3236ef7ba11f7e657e9
CocsAncel <corentin.ancel@edu.devinci.fr>
2021-01-11 16:40:34 +0100

    Adding footer

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,16 +1,58 @@
 <footer>
 
     <div id="footer-container">
-        <div>
-            <ul>
-                <?php foreach ($site->children()->listed() as $item) : ?>
-                    <li><a href="<?= $item->url() ?>"> <?= $item->title() ?></a>
+        <div id="footer-half">
+            <div>
+                <ul>
+                    <li>
+                        <a>A propos</a>
                     </li>
-                <?php endforeach ?>
-            </ul>
+
+                    <li>
+                        <a>Projets</a>
+                    </li>
+
+                    <li>
+                        <a>Services</a>
+                    </li>
+
+                    <li>
+                        <a>Partenariats</a>
+                    </li>
+                </ul>
+            </div>
+            <div>
+                <ul>
+                    <li>
+                        <a>Blog</a>
+                    </li>
+
+                    <li>
+                        <a>Contact</a>
+                    </li>
+                </ul>
+            </div>
+            <div>
+                <ul>
+                    <li><a>Politique de confidentialité</a></li>
+                    <li><a>Mentions légales</a></li>
+                    <li><a>FAQ</a></li>
+                </ul>
+            </div>
         </div>
-        <div></div>
-        <div></div>
+        <div id="right-footer">
+            <p>Inscrivez-vous à notre newsletter</p>
+            <input type="text" placeholder="Placeholder">
+            <button>Valider</button>
+            <div id="social-networks">
+                <img src="<?= url('assets/images/ic_FB.png') ?>">
+                <img src="<?= url('assets/images/ic_TT.png') ?>">
+                <img src="<?= url('assets/images/ic_TTV.png') ?>">
+                <img src="<?= url('assets/images/ic_YT.png') ?>">
+                <p>© GFBW 2020</p>
+            </div>
+        </div>
     </div>
+
 
 </footer>
